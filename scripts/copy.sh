066b1aaac30037ee456492ca9e7c849d5317558f
DevOps Guru <37587524+devopsguru91@users.noreply.github.com>
2022-10-15 21:41:08 +0530

    Update copy.sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,5 +2,5 @@
 ################################################################################################################
 # Please note this is an example copy path. You will need to change this according to the path on  your server.#
 ################################################################################################################
-rm -rf /usr/local/tomcat/webapps/*.war
+#rm -rf /usr/local/tomcat/webapps/*.war
 cp -R /opt/codedeploy-agent/deployment-root/*/*/deployment-archive/target/*.war /usr/local/tomcat/webapps/
